e1484d728f016422f138e3cf02ae7f518b965290
behrooz khan <kbalochkhan67@gmail.com>
2024-04-01 00:09:19 +0500

    Add Price Components in Main page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import About from '../components/About';
 import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
+import Price from '../components/Price';
 import Services from '../components/Services';
 
 const Home = () => {
@@ -17,6 +18,9 @@ const Home = () => {
         </div>
         <div>
           <Services/> 
+        </div>
+        <div>
+          <Price/>
         </div>
     </div>
   )
